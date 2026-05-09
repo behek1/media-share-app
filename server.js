553369badf4c297a3f973ea96c4a208eae2086a0
Behlul <behlul@example.com>
2026-05-09 19:40:18 +0300

    Fix volume mount path using absolute path

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (!fs.existsSync(uploadDir)) {
 // Set up storage engine for Multer
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+        cb(null, uploadDir);
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
